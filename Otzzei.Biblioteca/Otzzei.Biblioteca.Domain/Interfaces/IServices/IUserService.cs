8afdb863060f6e7e9c235294cdb5f32b2a0677cd
lucsantos21 <luc.olssay@outlook.com>
2023-05-17 16:19:45 -0300

    feat(users): Create UserService(CRUD: Post)

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Interfaces/IServices/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using Otzzei.Biblioteca.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Interfaces.IServices
+{
+    public interface IUserService
+    {
+        Task<Result<IdentityUser<Guid>>> CreateUserAsync(UserRequest request);
+    }
+}
---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using Otzzei.Biblioteca.Domain.Interfaces.IServices;
+using Otzzei.Biblioteca.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<IdentityUser<Guid>> _userManager;
+        public async Task<Result<IdentityUser<Guid>>> CreateUserAsync(UserRequest request)
+        {
+            var user = new IdentityUser<Guid>() { Email = request.Email, UserName = request.Email };
+            var create = await _userManager.CreateAsync(user);
+            if (create.Succeeded) return Result.Ok(user);
+            return Result.Fail("Falha ao criar um usuario!");
+        }
+    }
+}
