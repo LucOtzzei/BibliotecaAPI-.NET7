df12c4e22a4494d1c6171f102203855640750751
lucsantos21 <luc.olssay@outlook.com>
2023-05-17 15:50:21 -0300

    initial

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Application/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Otzzei.Biblioteca.Application
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+
+
+            services.AddMvc(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(IApplicationBuilder app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+
+    }
+}
