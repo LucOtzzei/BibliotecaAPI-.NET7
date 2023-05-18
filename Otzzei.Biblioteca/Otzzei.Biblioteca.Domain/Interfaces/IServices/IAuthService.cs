e68d27540cbb1b5deb08b77f10832180f5f84ca5
lucsantos21 <luc.olssay@outlook.com>
2023-05-18 10:26:43 -0300

    Feat(Auth): Create AuthService - register account password

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Interfaces/IServices/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+using Otzzei.Biblioteca.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Interfaces.IServices
+{
+    public interface IAuthService
+    {
+        Task<Result> CreateAccountPasswordAsync(string email, CreatePasswordRequest request);
+    }
+}
---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Otzzei.Biblioteca.Domain.Interfaces.IServices;
+using Otzzei.Biblioteca.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<IdentityUser<Guid>> _userManager;
+        public AuthService(UserManager<IdentityUser<Guid>> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<Result> CreateAccountPasswordAsync(string email, CreatePasswordRequest request)
+        {
+            var user = await GetUserByEmail(email);
+            if (user == null) return Result.Fail("User not found!");
+
+            var add = _userManager.AddPasswordAsync(user, request.Password).ToResult();
+            if (add.IsFailed) return Result.Fail("failed to register password");
+
+            return Result.Ok().WithSuccess("successfully registered password!");
+        }
+        private async Task<IdentityUser<Guid>> GetUserByEmail(string email)
+        {
+            return await _userManager.Users.FirstOrDefaultAsync(x => x.Email == email); 
+        }
+    }
+}
