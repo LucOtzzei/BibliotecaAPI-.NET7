c3f4a2d3b17567439bf8d7da341183689a6ab712
lucsantos21 <luc.olssay@outlook.com>
2023-06-22 13:36:36 -0300

    Feat(PublishingCompany): Create Entity of Publishing Company

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Entities/PublishingCompany.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Entities
+{
+    public class PublishingCompany
+    {
+        public PublishingCompany() { }
+        public PublishingCompany(PublishingCompanyRequest request)
+        {
+            Id = Guid.NewGuid();
+            Name = request.Name;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
