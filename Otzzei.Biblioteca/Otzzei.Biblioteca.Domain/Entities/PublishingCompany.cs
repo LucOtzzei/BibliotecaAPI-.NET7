0de9804aee0e8577d740990692c956ad00e86f65
lucsantos21 <luc.olssay@outlook.com>
2023-06-22 13:39:28 -0300

    Feat(PublishingCompany): Create a request of publishing company

--- Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Entities/PublishingCompany.cs
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Entities/PublishingCompany.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Otzzei.Biblioteca.Domain.Requests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Requests/PublishingCompanyRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Requests
+{
+    public class PublishingCompanyRequest
+    {
+        public string Name { get; set; }
+    }
+}
