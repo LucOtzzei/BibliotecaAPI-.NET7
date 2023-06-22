6e6431e8e58168d4488b0a01d7a57fbc458af724
lucsantos21 <luc.olssay@outlook.com>
2023-06-22 13:42:15 -0300

    Feat(PublishingCompany): Create Response of publishing company

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Responses/PublishingCompanyResponse.cs
@@ -0,0 +1,18 @@
+﻿using Otzzei.Biblioteca.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Responses
+{
+    public class PublishingCompanyResponse
+    {
+        public PublishingCompanyResponse(PublishingCompanyRequest request)
+        {
+            Name = request.Name;
+        }
+        public string Name { get; set; }
+    }
+}
