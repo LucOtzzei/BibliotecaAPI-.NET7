a9fb4090c84ac0a71d157370ce5ae9741050fe51
lucsantos21 <luc.olssay@outlook.com>
2023-06-27 10:33:40 -0300

    Feat(Book): Create a Entity and Enum of books

---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Entities
+{
+    public class Book
+    {
+        public Book() { }
+        public Book(BookRequest request)
+        {
+            Id = Guid.NewGuid();
+            Name = request.Name;
+            Genre = request.Genre;
+            AuthorId = request.AuthorId;
+            PublishingCompany = request.PublishedCompanyId;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public GenreEnum Genre { get; set; }
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+        public Guid PublishingCompanyId {  get; set; }
+        public PublishingCompany PublishingCompany { get; set; }
+    }
+}
---
+++ Otzzei.Biblioteca/Otzzei.Biblioteca.Domain/Enums/GenreEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.Biblioteca.Domain.Enums
+{
+    public enum GenreEnum
+    {
+        Comedy = 1,
+        Action = 2,
+        Drama = 3,
+        Fiction = 4,
+        Historical = 5,
+        Science = 6,
+        Philosophy = 7,
+        Sociology = 8,
+        Mathematics = 9,
+        Romance = 10
+    }
+}
